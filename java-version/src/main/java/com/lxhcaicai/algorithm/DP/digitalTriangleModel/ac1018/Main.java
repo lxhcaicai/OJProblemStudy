dc28cc53b35ccd0f9aad9b9753394cdef62ba6e5
lxhcaicai <2778763221@qq.com>
2023-09-23 22:52:40 +0800

    【数字三角形模型】AcWing 1018. 最低通行费 fix

--- java-version/src/main/java/com/lxhcaicai/algorithm/DP/digitalTriangleModel/ac1018/Main.java
+++ java-version/src/main/java/com/lxhcaicai/algorithm/DP/digitalTriangleModel/ac1018/Main.java
@@ -16,13 +16,18 @@ public class Main {
                 a[i][j] = scanner.nextInt();
             }
         }
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             Arrays.fill(f[i], N *N);
         }
         f[1][1] = a[1][1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n ; j++) {
-                f[i][j] = Math.min(f[i-1][j], f[i][j-1]) + a[i][j];
+                if(i > 1) {
+                    f[i][j] = Math.min (f[i][j], f[i-1][j] + a[i][j]);
+                }
+                if(j > 1) {
+                    f[i][j] = Math.min (f[i][j], f[i][j-1] + a[i][j]);
+                }
             }
         }
         System.out.println(f[n][n]);

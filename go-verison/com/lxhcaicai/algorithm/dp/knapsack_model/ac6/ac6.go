024e3cb38d1cd7d5761f432a09fcb9f37b309a17
lxhcaicai <2778763221@qq.com>
2023-10-15 16:19:20 +0800

    AcWing 6. 多重背包问题 III 【Go】

---
+++ go-verison/com/lxhcaicai/algorithm/dp/knapsack_model/ac6/ac6.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+// AcWing 6. 多重背包问题 III
+func main() {
+	var n, m int
+	fmt.Scan(&n, &m)
+	dp := make([]int, m+1)
+	pre := make([]int, m+1)
+	q := make([]int, m+1)
+	for i := 1; i <= n; i++ {
+		copyArr(pre, dp)
+		var s, v, w int
+		fmt.Scan(&v, &w, &s)
+		for j := 0; j < v; j++ {
+			head := 0
+			tail := -1
+			for k := j; k <= m; k += v {
+				if head <= tail && k-s*v > q[head] {
+					head++
+				}
+				for head <= tail && pre[q[tail]]-(q[tail]-j)/v*w <= pre[k]-(k-j)/v*w {
+					tail--
+				}
+				if head <= tail {
+					dp[k] = pre[q[head]] + (k-q[head])/v*w
+				}
+				tail += 1
+				q[tail] = k
+			}
+		}
+	}
+	fmt.Println(dp[m])
+}
+
+func copyArr(a, b []int) {
+	for i := range a {
+		a[i] = b[i]
+	}
+}

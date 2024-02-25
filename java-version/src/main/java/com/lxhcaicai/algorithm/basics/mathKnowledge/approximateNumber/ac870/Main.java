5669c545ddf38845a38f6f484ed31f0bdb147c60
lxhcaicai <2778763221@qq.com>
2024-02-26 01:05:35 +0800

    【数学知识】AcWing 870. 约数个数【约数】【Java】

---
+++ java-version/src/main/java/com/lxhcaicai/algorithm/basics/mathKnowledge/approximateNumber/ac870/Main.java
@@ -0,0 +1,49 @@
+package com.lxhcaicai.algorithm.basics.mathKnowledge.approximateNumber.ac870;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    static Map<Integer,Integer> countMap = new HashMap<>();
+    static final int MOD = (int) (1E9 + 7);
+
+    static void insert(int x, int num) {
+        if (countMap.containsKey(x)) {
+            countMap.put(x, countMap.get(x) + num);
+        } else {
+            countMap.put(x, num);
+        }
+    }
+
+    static void getDiv(int x) {
+        for(int i = 2; (long) i * i <= x; i ++) {
+            if (x % i == 0) {
+                int cnt = 0;
+                while (x % i == 0) {
+                    x /= i;
+                    cnt ++;
+                }
+                insert(i, cnt);
+            }
+        }
+        if (x > 1) {
+            insert(x, 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i ++) {
+            int x = scanner.nextInt();
+            getDiv(x);
+        }
+        long ans = 1;
+        for(Map.Entry<Integer,Integer> entry: countMap.entrySet()) {
+            ans = ans * (entry.getValue() + 1) % MOD;
+        }
+        System.out.println(ans);
+    }
+}

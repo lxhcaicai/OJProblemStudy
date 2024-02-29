bad453e630c5623374f9433de838205272cbe6cb
lxhcaicai <2778763221@qq.com>
2024-03-01 01:40:31 +0800

    【动态规划】AcWing 2. 01背包问题【背包问题】【Java】

---
+++ java-version/src/main/java/com/lxhcaicai/algorithm/basics/dynamicPlanning/ac905/Main.java
@@ -0,0 +1,24 @@
+package com.lxhcaicai.algorithm.basics.dynamicPlanning.ac905;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    static final int N = 1010;
+    static int[] f= new int[N];
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        for(int i = 1; i <= n; i ++) {
+            int v = scanner.nextInt();
+            int w = scanner.nextInt();
+            for(int j = m; j >=v; j --) {
+                f[j] = Math.max(f[j], f[j - v] + w);
+            }
+        }
+        System.out.println(Arrays.stream(f).max().getAsInt());
+    }
+}

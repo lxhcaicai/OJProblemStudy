69c6439ffb5738366bd3fcc51800a330a964b09e
lxhcaicai <2778763221@qq.com>
2024-06-02 17:01:18 +0800

    AcWing 787. 归并排序 【Go】

---
+++ go-verison/com/lxhcaicai/algorithm/basicAlgorithm/mergeSort/ac787/ac787.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+const N = 1e5 + 100
+
+var (
+	a [N]int
+	b [N]int
+)
+
+func mergeSort(l, r int) {
+	if l == r {
+		return
+	}
+	mid := (l + r) >> 1
+	mergeSort(l, mid)
+	mergeSort(mid+1, r)
+	i := l
+	j := mid + 1
+	for k := l; k <= r; k++ {
+		if j > r || (i <= mid && a[i] <= a[j]) {
+			b[k] = a[i]
+			i++
+		} else {
+			b[k] = a[j]
+			j++
+		}
+	}
+	for k := l; k <= r; k++ {
+		a[k] = b[k]
+	}
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	for i := 1; i <= n; i++ {
+		fmt.Scan(&a[i])
+	}
+	mergeSort(1, n)
+	for i := 1; i <= n; i++ {
+		fmt.Printf("%d ", a[i])
+	}
+}
